e971690541c75311131d3daf6da1ff1ee573e042
Andrej <andras.igneczi@gmail.com>
2019-11-28 13:19:54 +0100

    visitor vs dynamic_cast speed test

--- CppMemento/examples/visitor.cpp
+++ CppMemento/examples/visitor.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <memory>
+#include <chrono>
 
-#ifndef __SHARED_TEST__
+#define ITERATION 1e+8
+#ifdef __PTR_TEST__
 
 class Subject;
 class MyClass;
@@ -23,22 +25,32 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-    MyClass myClass;
-    Subject* subject = &myClass;
+    auto c1 = std::chrono::high_resolution_clock::now();
+    int counter = 0;
+    for(int i = 0; i < ITERATION; ++i) {
+        MyClass myClass;
+        Subject* subject = &myClass;
 
-    class MyVisitor : public Visitor {
-    public:
-        void visit(MyClass* myClass) override {
-            std::cout << "Subject is 'dynamic_casted' to MyClass: " << myClass << " without dynamic_cast\n";
-        }
-
-    };
-    MyVisitor visitor;
-    subject->accept(&visitor);
+        class MyVisitor : public Visitor {
+        public:
+            MyVisitor(int& counter) : counter(counter) {}
+            void visit(MyClass* myClass) override {
+                //std::cout << "Subject is 'dynamic_casted' to MyClass: " << myClass << " without dynamic_cast\n";
+                ++counter;
+            }
+        private:
+            int& counter;
+        };
+        MyVisitor visitor(counter);
+        subject->accept(&visitor);
+    }
+    std::cout << "counter: " << counter << "\n";
+    auto c2 = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(c2 - c1).count() << " microseconds\n";
     return 0;
 }
 
-#else
+#elif defined __SHARED_TEST__
 
 #include <iostream>
 #include <memory>
@@ -63,17 +75,57 @@ public:
 };
 
 int main(int argc, char* argv[]) {
-    std::shared_ptr<Subject> subject = std::make_shared<MyClass>();
+    auto c1 = std::chrono::high_resolution_clock::now();
+    int counter = 0;
+    for(int i = 0; i < ITERATION; ++i) {
+        std::shared_ptr<Subject> subject = std::make_shared<MyClass>();
 
-    class MyVisitor : public Visitor {
-    public:
-        void visit(std::shared_ptr<MyClass> myClass) override {
-            std::cout << "Subject is 'dynamic_casted' to MyClass: " << myClass.get() << " without dynamic_cast\n";
-        }
+        class MyVisitor : public Visitor {
+        public:
+            MyVisitor(int& counter) : counter(counter) {}
+            void visit(std::shared_ptr<MyClass> myClass) override {
+                //std::cout << "Subject is 'dynamic_casted' to MyClass: " << myClass.get() << " without dynamic_cast\n";
+                ++counter;
+            }
+        private:
+            int& counter;
+        };
+        MyVisitor visitor(counter);
+        subject->accept(visitor);
+    }
+    std::cout << "counter: " << counter << "\n";
+    auto c2 = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(c2 - c1).count() << " microseconds\n";
+    return 0;
+}
 
-    };
-    MyVisitor visitor;
-    subject->accept(visitor);
+#elif defined __DYNAMIC_TEST__
+class Subject {
+public:
+    virtual void accept() {}
+};
+
+class MyClass : public Subject {
+public:
+    MyClass(int& counter) : counter(counter) {}
+    void accept() override {
+        ++counter;
+    }
+    int& counter;
+};
+
+int main(int argc, char* argv[]) {
+    auto c1 = std::chrono::high_resolution_clock::now();
+    int counter = 0;
+    for(int i = 0; i < ITERATION; ++i) {
+        MyClass myClass(counter);
+        Subject* subject = &myClass;
+
+        dynamic_cast<MyClass*>(subject)->accept();
+    }
+    std::cout << "counter: " << counter << "\n";
+    auto c2 = std::chrono::high_resolution_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(c2 - c1).count() << " microseconds\n";
     return 0;
 }
 
